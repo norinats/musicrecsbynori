c6e5dab83a211ec106c5d86a886ec290341c17b9
Nori Natsuhara <norinatsuhara@u.boisestate.edu>
2020-09-29 17:15:44 -0600

    added comment box in song profile page

--- comments.php
+++ comments.php
@@ -3,5 +3,11 @@
         <link rel="stylesheet" type="text/css" href="css/comments.css">
     <?php require_once "header.php"; ?>
 
-        song comments page
+        <div class="song-profile">
+            
+        </div>
+
+        <form method="POST" action="comment_handler.php">
+            <textarea name="" id="" ></textarea>
+        </form>
     <?php require_once "footer.php"; ?>
