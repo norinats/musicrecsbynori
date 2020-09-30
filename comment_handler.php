b24ada9fbcd6ef6420e332fb4cebf08622941e04
Nori Natsuhara <norinatsuhara@u.boisestate.edu>
2020-09-30 17:50:49 -0600

    worked on comments page

---
+++ comment_handler.php
@@ -0,0 +1,4 @@
+<?php
+    header("Location: http://musicrecsbynori/comments.php");
+    exit();
+?>
--- comments.php
+++ comments.php
@@ -4,10 +4,17 @@
     <?php require_once "header.php"; ?>
 
         <div class="song-profile">
-            
+            <img src="images/favicon.png">
+            <div>
+                <h2>Song Name</h2>
+                <h4>Artist</h4>
+                <p>Recommendation/Request Description</p>
+            </div>
         </div>
 
         <form method="POST" action="comment_handler.php">
             <textarea name="" id="" ></textarea>
+            <br>
+            <input type="submit" value="post comment">
         </form>
     <?php require_once "footer.php"; ?>
